a5fef5b71c3a42ebe6de00d0a4c3766a660ca80a
AdamCSHU <79856216+AdamCSHU@users.noreply.github.com>
2021-03-04 15:13:08 +0000

    Added credits.
    Names, etc.

--- group project/src/Main.java
+++ group project/src/Main.java
@@ -2,4 +2,10 @@ public class Main {
     public static void main(String[] args) {
         
     }
+
+    public static void Application () {
+
+    }
 }
+
+// by Adam Campbell, Adam Jukes, Tom Drew, Jack Merritt and Matt "to be added".
